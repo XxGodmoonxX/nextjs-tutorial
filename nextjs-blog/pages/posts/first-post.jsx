53447caedca012de1c6b6626d2102bd5f80073cc
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-06-21 22:39:52 +0900

    polishing layout

--- nextjs-blog/pages/posts/first-post.jsx
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Head from "next/head";
-import Layout from '../../components/layout';
+import Layout, { siteTitle } from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <Layout>
       <Head>
-        <title>First Post</title>
+        <title>{siteTitle}</title>
       </Head>
       <h1>First Post</h1>
       <h2>
