1c85055a69836a71d346e518fa150a339bfb5f63
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-07-26 12:11:19 +0900

    import date-fns, next/link and beautify

--- nextjs-blog/pages/index.jsx
+++ nextjs-blog/pages/index.jsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Date from "date-fns";
+import Link from "next/link";
 
 export default function Home({ allPostsData }) {
   return (
@@ -20,12 +22,12 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({id, date, title}) => (
+          {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               {title}
-              <br/>
+              <br />
               {id}
-              <br/>
+              <br />
               {date}
             </li>
           ))}
